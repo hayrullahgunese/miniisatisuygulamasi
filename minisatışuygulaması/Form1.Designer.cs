fdb37056978dcb1d3d577d6effe39d5fcae20bc0
hayrullahguns <hayrullahgunes46@gmail.com>
2024-12-12 17:56:46 +0300

    221120231021_mini_satis_uygulamasi_hayrullah_gunes

---
+++ minisatışuygulaması/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace minisatışuygulaması
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+       
+       
+       
+        ///<param name"disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxadetgir = new System.Windows.Forms.TextBox();
+            this.comboBoxfidanseç = new System.Windows.Forms.ComboBox();
+            this.dataGridViewfidan = new System.Windows.Forms.DataGridView();
+            this.fidanDataSet = new minisatışuygulaması.fidanDataSet();
+            this.fidanBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.fidanTableAdapter = new minisatışuygulaması.fidanDataSetTableAdapters.fidanTableAdapter();
+            this.fidanidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fidanadiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fidanfiyatiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.buttonalışverişitamamla = new System.Windows.Forms.Button();
+            this.dataGridViewsatislar = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.fidanDataSet1 = new minisatışuygulaması.fidanDataSet1();
+            this.satislarBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.satislarTableAdapter = new minisatışuygulaması.fidanDataSet1TableAdapters.satislarTableAdapter();
+            this.satisidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.satilanfidanidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.satilanfidanadetiDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.toplamtutarDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fidanDataSet2 = new minisatışuygulaması.fidanDataSet2();
+            this.fidanBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.fidanTableAdapter1 = new minisatışuygulaması.fidanDataSet2TableAdapters.fidanTableAdapter();
+            this.fidanBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.fidanBindingSource3 = new System.Windows.Forms.BindingSource(this.components);
+            this.fidanDataSet3 = new minisatışuygulaması.fidanDataSet3();
+            this.fidanBindingSource4 = new System.Windows.Forms.BindingSource(this.components);
+            this.fidanTableAdapter2 = new minisatışuygulaması.fidanDataSet3TableAdapters.fidanTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewfidan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewsatislar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.satislarBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource4)).BeginInit();
+            this.SuspendLayout();
+            
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(375, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Fidan Seçiniz";
+                        this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(385, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Adet Giriniz";
+            
+            this.textBoxadetgir.Location = new System.Drawing.Point(451, 91);
+            this.textBoxadetgir.Name = "textBoxadetgir";
+            this.textBoxadetgir.Size = new System.Drawing.Size(140, 20);
+            this.textBoxadetgir.TabIndex = 2;
+             
+            this.comboBoxfidanseç.FormattingEnabled = true;
+            this.comboBoxfidanseç.Location = new System.Drawing.Point(451, 64);
+            this.comboBoxfidanseç.Name = "comboBoxfidanseç";
+            this.comboBoxfidanseç.Size = new System.Drawing.Size(140, 21);
+            this.comboBoxfidanseç.TabIndex = 3;
+            
+            this.dataGridViewfidan.AutoGenerateColumns = false;
+            this.dataGridViewfidan.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewfidan.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.fidanidDataGridViewTextBoxColumn,
+            this.fidanadiDataGridViewTextBoxColumn,
+            this.fidanfiyatiDataGridViewTextBoxColumn});
+            this.dataGridViewfidan.DataSource = this.fidanBindingSource4;
+            this.dataGridViewfidan.Location = new System.Drawing.Point(18, 64);
+            this.dataGridViewfidan.Name = "dataGridViewfidan";
+            this.dataGridViewfidan.Size = new System.Drawing.Size(351, 135);
+            this.dataGridViewfidan.TabIndex = 4;
+             
+            this.fidanDataSet.DataSetName = "fidanDataSet";
+            this.fidanDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+             
+            this.fidanBindingSource.DataMember = "fidan";
+            this.fidanBindingSource.DataSource = this.fidanDataSet;
+            
+            this.fidanTableAdapter.ClearBeforeFill = true;
+            
+            this.fidanidDataGridViewTextBoxColumn.DataPropertyName = "fidanid";
+            this.fidanidDataGridViewTextBoxColumn.HeaderText = "fidanid";
+            this.fidanidDataGridViewTextBoxColumn.Name = "fidanidDataGridViewTextBoxColumn";
+            this.fidanidDataGridViewTextBoxColumn.ReadOnly = true;
+            
+            this.fidanadiDataGridViewTextBoxColumn.DataPropertyName = "fidanadi";
+            this.fidanadiDataGridViewTextBoxColumn.HeaderText = "fidanadi";
+            this.fidanadiDataGridViewTextBoxColumn.Name = "fidanadiDataGridViewTextBoxColumn";
+            
+            this.fidanfiyatiDataGridViewTextBoxColumn.DataPropertyName = "fidanfiyati";
+            this.fidanfiyatiDataGridViewTextBoxColumn.HeaderText = "fidanfiyati";
+            this.fidanfiyatiDataGridViewTextBoxColumn.Name = "fidanfiyatiDataGridViewTextBoxColumn";
+             
+            this.buttonalışverişitamamla.BackColor = System.Drawing.Color.Cyan;
+            this.buttonalışverişitamamla.Location = new System.Drawing.Point(451, 135);
+            this.buttonalışverişitamamla.Name = "buttonalışverişitamamla";
+            this.buttonalışverişitamamla.Size = new System.Drawing.Size(139, 26);
+            this.buttonalışverişitamamla.TabIndex = 5;
+            this.buttonalışverişitamamla.Text = "Alışverişi Tamamla";
+            this.buttonalışverişitamamla.UseVisualStyleBackColor = false;
+            this.buttonalışverişitamamla.Click += new System.EventHandler(this.buttonalışverişitamamla_Click);
+             
+            this.dataGridViewsatislar.AutoGenerateColumns = false;
+            this.dataGridViewsatislar.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewsatislar.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.satisidDataGridViewTextBoxColumn,
+            this.satilanfidanidDataGridViewTextBoxColumn,
+            this.satilanfidanadetiDataGridViewTextBoxColumn,
+            this.toplamtutarDataGridViewTextBoxColumn});
+            this.dataGridViewsatislar.DataSource = this.satislarBindingSource;
+            this.dataGridViewsatislar.Location = new System.Drawing.Point(18, 450);
+            this.dataGridViewsatislar.Name = "dataGridViewsatislar";
+            this.dataGridViewsatislar.Size = new System.Drawing.Size(447, 134);
+            this.dataGridViewsatislar.TabIndex = 6;
+             
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(18, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Fidan Çeşitleri ve Fiyatı";
+            
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Location = new System.Drawing.Point(18, 432);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Yapılan Satışlar";
+            
+            this.fidanDataSet1.DataSetName = "fidanDataSet1";
+            this.fidanDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+             
+            this.satislarBindingSource.DataMember = "satislar";
+            this.satislarBindingSource.DataSource = this.fidanDataSet1;
+            
+            this.satislarTableAdapter.ClearBeforeFill = true;
+             
+            this.satisidDataGridViewTextBoxColumn.DataPropertyName = "satisid";
+            this.satisidDataGridViewTextBoxColumn.HeaderText = "satisid";
+            this.satisidDataGridViewTextBoxColumn.Name = "satisidDataGridViewTextBoxColumn";
+            this.satisidDataGridViewTextBoxColumn.ReadOnly = true;
+            
+            this.satilanfidanidDataGridViewTextBoxColumn.DataPropertyName = "satilanfidanid";
+            this.satilanfidanidDataGridViewTextBoxColumn.HeaderText = "satilanfidanid";
+            this.satilanfidanidDataGridViewTextBoxColumn.Name = "satilanfidanidDataGridViewTextBoxColumn";
+            
+            this.satilanfidanadetiDataGridViewTextBoxColumn.DataPropertyName = "satilanfidanadeti";
+            this.satilanfidanadetiDataGridViewTextBoxColumn.HeaderText = "satilanfidanadeti";
+            this.satilanfidanadetiDataGridViewTextBoxColumn.Name = "satilanfidanadetiDataGridViewTextBoxColumn";
+             
+            this.toplamtutarDataGridViewTextBoxColumn.DataPropertyName = "toplamtutar";
+            this.toplamtutarDataGridViewTextBoxColumn.HeaderText = "toplamtutar";
+            this.toplamtutarDataGridViewTextBoxColumn.Name = "toplamtutarDataGridViewTextBoxColumn";
+             
+            this.fidanDataSet2.DataSetName = "fidanDataSet2";
+            this.fidanDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            
+            this.fidanBindingSource1.DataMember = "fidan";
+            this.fidanBindingSource1.DataSource = this.fidanDataSet2;
+            
+            this.fidanTableAdapter1.ClearBeforeFill = true;
+            
+            this.fidanBindingSource2.DataMember = "fidan";
+            this.fidanBindingSource2.DataSource = this.fidanDataSet;
+            
+            this.fidanBindingSource3.DataMember = "fidan";
+            this.fidanBindingSource3.DataSource = this.fidanDataSet2;
+             
+            this.fidanDataSet3.DataSetName = "fidanDataSet3";
+            this.fidanDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+             
+            this.fidanBindingSource4.DataMember = "fidan";
+            this.fidanBindingSource4.DataSource = this.fidanDataSet3;
+             
+            this.fidanTableAdapter2.ClearBeforeFill = true;
+             
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1019, 698);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewsatislar);
+            this.Controls.Add(this.buttonalışverişitamamla);
+            this.Controls.Add(this.dataGridViewfidan);
+            this.Controls.Add(this.comboBoxfidanseç);
+            this.Controls.Add(this.textBoxadetgir);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewfidan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewsatislar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.satislarBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanDataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fidanBindingSource4)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxadetgir;
+        private System.Windows.Forms.ComboBox comboBoxfidanseç;
+        private System.Windows.Forms.DataGridView dataGridViewfidan;
+        private fidanDataSet fidanDataSet;
+        private System.Windows.Forms.BindingSource fidanBindingSource;
+        private fidanDataSetTableAdapters.fidanTableAdapter fidanTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fidanidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fidanadiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fidanfiyatiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.Button buttonalışverişitamamla;
+        private System.Windows.Forms.DataGridView dataGridViewsatislar;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private fidanDataSet1 fidanDataSet1;
+        private System.Windows.Forms.BindingSource satislarBindingSource;
+        private fidanDataSet1TableAdapters.satislarTableAdapter satislarTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn satisidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn satilanfidanidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn satilanfidanadetiDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn toplamtutarDataGridViewTextBoxColumn;
+        private fidanDataSet2 fidanDataSet2;
+        private System.Windows.Forms.BindingSource fidanBindingSource1;
+        private fidanDataSet2TableAdapters.fidanTableAdapter fidanTableAdapter1;
+        private System.Windows.Forms.BindingSource fidanBindingSource2;
+        private System.Windows.Forms.BindingSource fidanBindingSource3;
+        private fidanDataSet3 fidanDataSet3;
+        private System.Windows.Forms.BindingSource fidanBindingSource4;
+        private fidanDataSet3TableAdapters.fidanTableAdapter fidanTableAdapter2;
+    }
+}
+
---
+++ minisatışuygulaması/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Drawing.Printing; 
+using System.Windows.Forms;
+
+namespace minisatışuygulaması
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn;
+        private string connectionString = "Server=DESKTOP-8FACNE8; Database=fidan; Integrated Security=True;";
+        private int selectedFidanId;
+        private string selectedFidanAdi;
+        private int adet;
+        private decimal toplamTutar;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.fidanTableAdapter2.Fill(this.fidanDataSet3.fidan);
+            LoadFidanData();
+            LoadSalesData();
+        }
+
+        private void LoadFidanData()
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM fidan", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                comboBoxfidanseç.DataSource = dt;
+                comboBoxfidanseç.DisplayMember = "fidanadi";
+                comboBoxfidanseç.ValueMember = "fidanid";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Veri yüklenirken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void LoadSalesData()
+        {
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM satislar", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                dataGridViewsatislar.DataSource = dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Satış verileri yüklenirken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private void buttonalışverişitamamla_Click(object sender, EventArgs e)
+        {
+            selectedFidanId = Convert.ToInt32(comboBoxfidanseç.SelectedValue);
+            selectedFidanAdi = comboBoxfidanseç.Text;
+            adet = Convert.ToInt32(textBoxadetgir.Text);
+            decimal fidanFiyati = GetFidanPrice(selectedFidanId);
+
+            toplamTutar = adet * fidanFiyati;
+
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("INSERT INTO satislar (satilanfidanid, satilanfidanadeti, toplamtutar) VALUES (@fidanId, @adet, @toplamTutar)", conn);
+                cmd.Parameters.AddWithValue("@fidanId", selectedFidanId);
+                cmd.Parameters.AddWithValue("@adet", adet);
+                cmd.Parameters.AddWithValue("@toplamTutar", toplamTutar);
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+                conn.Close();
+
+                MessageBox.Show("Satış başarılı!");
+
+                LoadSalesData();
+
+                PrintReceipt();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Satış kaydı eklenirken bir hata oluştu: " + ex.Message);
+            }
+        }
+
+        private decimal GetFidanPrice(int fidanId)
+        {
+            decimal price = 0;
+            try
+            {
+                conn = new SqlConnection(connectionString);
+                SqlCommand cmd = new SqlCommand("SELECT fidanfiyati FROM fidan WHERE fidanid = @fidanId", conn);
+                cmd.Parameters.AddWithValue("@fidanId", fidanId);
+
+                conn.Open();
+                price = Convert.ToDecimal(cmd.ExecuteScalar());
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Fidan fiyatı alınırken bir hata oluştu: " + ex.Message);
+            }
+
+            return price;
+        }
+
+        private void PrintReceipt()
+        {
+            PrintDocument printDocument = new PrintDocument();
+            printDocument.PrintPage += new PrintPageEventHandler(PrintDocument_PrintPage);
+            PrintPreviewDialog printPreviewDialog = new PrintPreviewDialog();
+            printPreviewDialog.Document = printDocument;
+            printPreviewDialog.ShowDialog();
+        }
+
+        private void PrintDocument_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            Font font = new Font("Arial", 12);
+            Brush brush = Brushes.Black;
+
+            int yPos = 20;
+            int lineHeight = 25;
+
+            g.DrawString("Fidan Satış Fişi", new Font("Arial", 16, FontStyle.Bold), brush, 100, yPos);
+            yPos += lineHeight;
+
+            g.DrawString("Fidan Adı: " + selectedFidanAdi, font, brush, 20, yPos);
+            yPos += lineHeight;
+
+            g.DrawString("Adet: " + adet, font, brush, 20, yPos);
+            yPos += lineHeight;
+
+            g.DrawString("Fiyat: " + GetFidanPrice(selectedFidanId).ToString("C"), font, brush, 20, yPos);
+            yPos += lineHeight;
+
+            g.DrawString("Toplam Tutar: " + toplamTutar.ToString("C"), font, brush, 20, yPos);
+            yPos += lineHeight;
+
+            g.DrawLine(Pens.Black, 20, yPos, 280, yPos);
+            yPos += 10;
+
+            g.DrawString("Teşekkür ederiz!", font, brush, 20, yPos);
+        }
+    }
+}
